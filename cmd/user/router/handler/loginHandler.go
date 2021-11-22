4acbc3f5b19a901c5b0dc12cfacdbcd4653394a7
lihao <568330385@qq.com>
2021-11-22 22:33:03 +0800

    .

--- cmd/user/router/handler/loginHandler.go
+++ cmd/user/router/handler/loginHandler.go
@@ -3,8 +3,6 @@ package handler
 import (
 	"github.com/gin-gonic/gin"
 	jwt "github.com/go-admin-team/go-admin-core/sdk/pkg/jwtauth"
-	"github.com/go-magic/rook/pkg/api/database/mysql/user"
-	"github.com/go-magic/rook/pkg/api/middleware"
 )
 
 func Login(ctx *gin.Context) {
@@ -12,26 +10,5 @@ func Login(ctx *gin.Context) {
 }
 
 func registerSysUserRouter(v1 *gin.RouterGroup, authMiddleware *jwt.GinJWTMiddleware) {
-	api := user.User{}
-	r := v1.Group("/sys-user").Use(authMiddleware.MiddlewareFunc()).Use(middleware.AuthCheckRole()).Use(actions.PermissionAction())
-	{
-		r.GET("", api.GetPage)
-		r.GET("/:id", api.Get)
-		r.POST("", api.Insert)
-		r.PUT("", api.Update)
-		r.DELETE("", api.Delete)
-	}
 
-	user := v1.Group("/user").Use(authMiddleware.MiddlewareFunc()).Use(middleware.AuthCheckRole()).Use(actions.PermissionAction())
-	{
-		user.GET("/profile", api.GetProfile)
-		user.POST("/avatar", api.InsetAvatar)
-		user.PUT("/pwd/set", api.UpdatePwd)
-		user.PUT("/pwd/reset", api.ResetPwd)
-		user.PUT("/status", api.UpdateStatus)
-	}
-	v1auth := v1.Group("").Use(authMiddleware.MiddlewareFunc())
-	{
-		v1auth.GET("/getinfo", api.GetInfo)
-	}
 }
