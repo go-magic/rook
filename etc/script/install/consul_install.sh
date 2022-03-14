574723df1219a1f3afde47c49ab0a0c037cf4ec7
lihao <568330385@qq.com>
2022-03-14 17:31:51 +0800

    删除多余脚本

--- etc/script/install/consul_install.sh
+++ etc/script/install/consul_install.sh
@@ -18,5 +18,3 @@ docker run -d -p 8500:8500 -p 8300:8300 -p 8301:8301 -p 8302:8302 -p 8600:8600 \
 	-node='192.168.164.134' -bind='0.0.0.0' -advertise='192.168.164.134' \
 	 -join=192.168.164.132
 
-#install client
-docker run -d -p 8500:8500 -p 8300:8300 -p 8301:8301 -p 8302:8302 -p 8600:8600 --name=consul_client1 -v /root/lihao/consul/consul.json:/etc/consul/consul.json  consul agent -config-file=/etc/consul/consul.json -ui -node=client1 -client=0.0.0.0 -join=172.17.0.2
