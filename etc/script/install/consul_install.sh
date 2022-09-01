fe01fd6b64bcf0401a77a9ff1d367cfdf2c4153c
lihao <568330385@qq.com>
2022-09-01 09:45:36 +0800

    指定脚本

--- etc/script/install/consul_install.sh
+++ etc/script/install/consul_install.sh
@@ -7,24 +7,24 @@
 #-advertise对外暴露的ip地址
 # 安装consul集群
 docker run -d -p 8500:8500 -p 8300:8300 -p 8301:8301 -p 8302:8302 -p 8600:8600 \
-	--restart=always --name=consul_server1 consul:latest agent \
+	--restart=always --name=consul_server1 consul:1.13.1 agent \
 	-server -bootstrap-expect=3 -ui -node=server1 -client='0.0.0.0' \
 	-node='192.168.164.132' -bind='0.0.0.0' -advertise='192.168.164.132'
 
 docker run -d -p 8500:8500 -p 8300:8300 -p 8301:8301 -p 8302:8302 -p 8600:8600 \
-	--restart=always --name=consul_server2 consul:latest agent \
+	--restart=always --name=consul_server2 consul:1.13.1 agent \
 	-server -bootstrap-expect=3 -ui -node=server2 -client='0.0.0.0' \
 	-node='192.168.164.133' -bind='0.0.0.0' -advertise='192.168.164.133' \
 	 -join=192.168.164.132
 
 docker run -d -p 8500:8500 -p 8300:8300 -p 8301:8301 -p 8302:8302 -p 8600:8600 \
-	--restart=always --name=consul_server3 consul:latest agent \
+	--restart=always --name=consul_server3 consul:1.13.1 agent \
 	-server -bootstrap-expect=3 -ui -node=server3 -client='0.0.0.0' \
 	-node='192.168.164.134' -bind='0.0.0.0' -advertise='192.168.164.134' \
 	 -join=192.168.164.132
 
 #单机版
 docker run -d -p 8500:8500 -p 8300:8300 -p 8301:8301 -p 8302:8302 -p 8600:8600 \
-	--restart=always --name=consul consul:latest agent \
+	--restart=always --name=consul consul:1.13.1 agent \
 	-server -bootstrap -ui -node=node1 -client='0.0.0.0'
 
--- etc/script/install/mysql_install.sh
+++ etc/script/install/mysql_install.sh
@@ -3,4 +3,4 @@
 #privileged 获取root权限
 docker run --name=mysql --restart=always --privileged=true \
           -p 3306:3306 -v /opt/mysql/data:/var/lib/mysql \
-          -e MYSQL_ROOT_PASSWORD="aU0FEQ^#ad1" -d mysql
+          -e MYSQL_ROOT_PASSWORD="aU0FEQ^#ad1" -d mysql:5.7.39
